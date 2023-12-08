b5c998ff4a5635065cff17bfa850931982583991
akshanshthakur2 <contact.akshansh0@gmail.com>
2023-12-09 04:23:44 +0530

    Responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,28 +6,35 @@ import 'react-toastify/dist/ReactToastify.css';
 const Home = () => {
   const [data, setdata] = useContext(userinfo)
   const [name, setname] = useState("")
-  const [role, setrole] = useState("")
+  const [role, setrole] = useState("Admin")
 
   localStorage.setItem("data", JSON.stringify(data))
 
-  const notify = () => toast("Data Added!")
+  const added = () => toast("Data Added!")
+  const invalid = () => toast("Enter Name!")
 
   const submitHandler = (e) => {
     e.preventDefault()
-    const copy = [...data]
-    copy.push({ name, role })
-    setdata(copy)
+    if (name === ""){
+      // toast("Input name!")
+      console.log("enter input")
+    }else{
+      const copy = [...data]
+      copy.push({ name, role })
+      setdata(copy)
+    }
   }
   return (
     <div>
       <div id="form">
-
-
         <form onSubmit={submitHandler}>
           <h1>Insert Users Info!</h1>
-          <input onChange={(e) => setname(e.target.value)} type="text" placeholder='Name' />
-          <input onChange={(e) => setrole(e.target.value)} type="text" placeholder='Role' />
-          <button type='submit' onClick={notify}>Submit</button>
+          <input onChange={(e) => setname(e.target.value)} value={name} type="text" placeholder='Name' />
+          <select onChange={(e) => setrole(e.target.value)} value={role} name="role">
+            <option value="Admin">Admin</option>
+            <option value="User">User</option>
+          </select>
+          <button type='submit' onClick={name === '' ? invalid : added}>Submit</button>
           <ToastContainer position="top-center"
             autoClose={5000}
             hideProgressBar={false}
